fcdd9c4fe704f3e205038f2b67728d4acdffaa1b
Ademola Balogun <abalogun@softalliance.co.ng>
2023-12-20 15:11:39 +0100

    first commit

---
+++ number3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+public class program
+{
+
+    public static void Main()
+    {
+        var list = new List<int>();
+
+        while (list.Count < 5)
+        {
+            Console.WriteLine("Type 5 unique numbers:");
+            var input = Convert.ToInt32(Console.ReadLine());
+
+            if (list.Contains(input))
+                Console.WriteLine("Error! Number already in list!");
+            else
+                list.Add(input);
+        }
+
+        list.Sort();
+        foreach (var number in list)
+        {
+            Console.Write("{0} ", number);
+        }
+    }
+}
